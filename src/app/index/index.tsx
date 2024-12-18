97eb6f12a7b41ea7b5d504dbf1b5f2529a663702
markgomer <aureliojuniorcmrj@hotmail.com>
2024-12-18 12:03:16 -0300

    Add logo and plus sign

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,26 @@
-import { Text, View } from "react-native"
+import { View, Image, TouchableOpacity } from "react-native"
 
 import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+import { MaterialIcons } from "@expo/vector-icons"
+
 
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Hello</Text>
-            <Text style={styles.title}>React Native!</Text>
+            <View style={styles.header}>
+                <Image
+                    source={require("@/assets/logo.png")}
+                    style={styles.logo}
+                />
+                <TouchableOpacity activeOpacity={0.5}>
+                    <MaterialIcons
+                        name="add"
+                        size={32}
+                        color={colors.green[300]}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
